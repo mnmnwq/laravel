75dea9c569145b87182fd69cd2e1fd11086e9885
mnmnwq <mn2er123@163.com>
2016-09-22 20:53:03 +0800

    home

--- blog/app/Http/Controllers/Admin/ArticleController.php
+++ blog/app/Http/Controllers/Admin/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\admin;
 use App\Http\Model\Article;
+use App\Http\Model\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,9 +12,11 @@ use App\Http\Requests;
 class ArticleController extends CommonController
 {
     protected $art_obj;
+    protected $cate_obj;
     public function __construct()
     {
         $this->art_obj = new Article();
+        $this->cate_obj = new Category();
     }
     //get.admin/article  全部文章列表
     public function index(){
@@ -22,7 +25,7 @@ class ArticleController extends CommonController
 
     //get.admin/article/create  添加文章
     public function create(){
-        $data = [];
+        $data = $this->cate_obj->tree();
         return view('admin.article.add',compact('data'));
     }
 }
--- blog/resources/views/admin/article/add.blade.php
+++ blog/resources/views/admin/article/add.blade.php
@@ -24,49 +24,67 @@
             <table class="add_tab">
                 <tbody>
                     <tr>
-                        <th width="120"><i class="require">*</i>父级分类：</th>
+                        <th width="120">分类：</th>
                         <td>
-                            <select name="cate_pid">
-                                <option value="0">==顶级分类==</option>
+                            <select name="cate_id">
                                 @foreach($data as $k=>$v)
-                                <option value="{{$v['cate_id']}}">{{$v['cate_name']}}</option>
+                                <option value="{{$v['cate_id']}}">{{$v['_cate_name']}}</option>
                                 @endforeach
                             </select>
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>分类名称：</th>
+                        <th>文章标题：</th>
                         <td>
-                            <input type="text" name="cate_name" ng-required="true">
-                            <span><i class="fa fa-exclamation-circle yellow"></i>分类名称必须填写</span>
+                            <input type="text" class="lg" name="art_title">
+                            {{--<p>标题可以写30个字</p>--}}
                         </td>
                     </tr>
                     <tr>
-                        <th>分类标题：</th>
+                        <th>编辑：</th>
                         <td>
-                            <input type="text" class="lg" name="cate_title">
+                            <input type="text" class="sm" name="art_editor" ng-required="true">
+                            {{--<span><i class="fa fa-exclamation-circle yellow"></i>这里是短文本长度</span>--}}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>缩略图：</th>
+                        <td>
+                            <input type="text" size="50" name="art_thumb">
                             {{--<p>标题可以写30个字</p>--}}
                         </td>
                     </tr>
                     <tr>
                         <th>关键词：</th>
                         <td>
-                            <textarea name="cate_keywords"></textarea>
+                            <input type="text" class="lg" name="art_keywords">
                         </td>
                     </tr>
                     <tr>
-                        <th>关键词描述：</th>
+                        <th>描述：</th>
                         <td>
-                            <textarea name="cate_description"></textarea>
+                            <textarea name="art_description"></textarea>
                         </td>
                     </tr>
                     <tr>
-                        <th><i class="require">*</i>排序：</th>
+                        <th>文章内容：</th>
                         <td>
-                            <input type="text" class="sm" name="cate_order" ng-required="true">
-                            {{--<span><i class="fa fa-exclamation-circle yellow"></i>这里是短文本长度</span>--}}
+                        <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/ueditor.config.js')}}"></script>
+                            <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/ueditor.all.min.js')}}"> </script>
+                            <script type="text/javascript" charset="utf-8" src="{{asset('resources/org/ueditor/lang/zh-cn/zh-cn.js')}}"></script>
+                            <script id="editor" name="art_content" type="text/plain" style="width:860px;height:500px;"></script>
+                            <script type="text/javascript">
+                                    var ue = UE.getEditor('editor');
+                            </script>
+                            <style>
+                                .edui-default{line-height: 28px;}
+                                div.edui-combox-body,div.edui-button-body,div.edui-splitbutton-body
+                                {overflow: hidden; height:20px;}
+                                div.edui-box{overflow: hidden; height:22px;}
+                            </style>
                         </td>
                     </tr>
+
                     <tr>
                         <th></th>
                         <td>
